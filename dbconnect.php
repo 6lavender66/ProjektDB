31aad2eae94532c9607ddf2e9f8214bd9da890db
Bartosz Szczepaniok <129668966+6lavender66@users.noreply.github.com>
2023-06-09 20:02:28 +0200

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+	$host="localhost"; 		// Nazwa hosta
+	$user="root"; 			// Nazwa uzytkownika mysql
+	$password=""; 			// Haslo do bazy, jeśli coś by było jednak to "1234"
+	$database="ksiegarnia"; // Nazwa bazy
+	$table1="klienci"; 		
+	$table2="komiks"; 		//Nazwa tabeli 
+	$table3="zamowienia"; 	
+?>
+
