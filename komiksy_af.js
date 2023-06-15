d7cfe493ea8c07b94514e565bc29f43a724d0128
Bartosz Szczepaniok <129668966+6lavender66@users.noreply.github.com>
2023-06-15 21:47:38 +0200

    Add files via upload

---
+++ komiksy_af.js
@@ -0,0 +1,262 @@
+[
+    
+    {
+        "tytuł": "Amazing Adventures (Vol 1) #1",
+        "numer": 1,
+        "data_wydania": "07-03-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adventures (Vol 1) #2",
+        "numer": 2,
+        "data_wydania": "06-04-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adventures (Vol 1) #3",
+        "numer": 3,
+        "data_wydania": "09-05-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adventures (Vol 1) #4",
+        "numer": 4,
+        "data_wydania": "01-06-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adventures (Vol 1) #5",
+        "numer": 5,
+        "data_wydania": "05-07-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adventures (Vol 1) #6",
+        "numer": 6,
+        "data_wydania": "01-08-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #7",
+        "numer": 7,
+        "data_wydania": "31-08-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #8",
+        "numer": 8,
+        "data_wydania": "26-09-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #9",
+        "numer": 9,
+        "data_wydania": "02-11-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #10",
+        "numer": 10,
+        "data_wydania": "05-12-1961",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #11",
+        "numer": 11,
+        "data_wydania": "02-01-1962",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #12",
+        "numer": 12,
+        "data_wydania": "01-02-1962",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #13",
+        "numer": 13,
+        "data_wydania": "01-03-1962",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Adult Fantasy (Vol 1) #14",
+        "numer": 14,
+        "data_wydania": "03-04-1962",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol 1) #15",
+        "numer": 15,
+        "data_wydania": "05-06-1962",
+        "autor": "Stan Lee"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol 1) #16",
+        "numer": 16,
+        "data_wydania": "09-11-1995",
+        "autor": "Bob Budiansky"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol 1) #17",
+        "numer": 17,
+        "data_wydania": "21-12-1995",
+        "autor": "Bob Budiansky"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol 1) #18",
+        "numer": 18,
+        "data_wydania": "15-02-1996",
+        "autor": "Bob Budiansky"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #1",
+        "numer": 1,
+        "data_wydania": "30-06-2004",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #2",
+        "numer": 2,
+        "data_wydania": "04-08-2004",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #3",
+        "numer": 3,
+        "data_wydania": "25-08-2004",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #4",
+        "numer": 4,
+        "data_wydania": "29-09-2004",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #5",
+        "numer": 5,
+        "data_wydania": "27-10-2004",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #6",
+        "numer": 6,
+        "data_wydania": "24-11-2004",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #7",
+        "numer": 7,
+        "data_wydania": "06-04-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #8",
+        "numer": 8,
+        "data_wydania": "04-05-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #9",
+        "numer": 9,
+        "data_wydania": "02-06-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #10",
+        "numer": 10,
+        "data_wydania": "07-07-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #11",
+        "numer": 11,
+        "data_wydania": "03-08-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #12",
+        "numer": 12,
+        "data_wydania": "08-09-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #13",
+        "numer": 13,
+        "data_wydania": "05-10-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #14",
+        "numer": 14,
+        "data_wydania": "26-10-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #15",
+        "numer": 15,
+        "data_wydania": "30-11-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #16",
+        "numer": 16,
+        "data_wydania": "28-12-2005",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #17",
+        "numer": 17,
+        "data_wydania": "25-01-2006",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #18",
+        "numer": 18,
+        "data_wydania": "01-03-2006",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #19",
+        "numer": 19,
+        "data_wydania": "22-03-2006",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 2) #20",
+        "numer": 20,
+        "data_wydania": "26-04-2006",
+        "autor": "Joe Quesada"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 3) #1",
+        "numer": 1,
+        "data_wydania": "28-07-2021",
+        "autor": "C.B. Cebulski"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 3) #2",
+        "numer": 2,
+        "data_wydania": "25-08-2021",
+        "autor": "C.B. Cebulski"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 3) #3",
+        "numer": 3,
+        "data_wydania": "29-09-2021",
+        "autor": "C.B. Cebulski"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 3) #4",
+        "numer": 4,
+        "data_wydania": "03-11-2021",
+        "autor": "C.B. Cebulski"
+      },
+      {
+        "tytuł": "Amazing Fantasy (Vol. 3) #5",
+        "numer": 5,
+        "data_wydania": "08-12-2021",
+        "autor": "C.B. Cebulski"
+      }
+]
+
